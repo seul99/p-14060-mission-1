a425338817688b98a1a01bd68bfd57d5657f00c1
sia <piao516@naver.com>
2025-11-03 11:05:13 +0900

    feat:2단계 완료

--- src/main/java/com/back/Main.java
+++ src/main/java/com/back/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("== 명언 앱 ==");
         Scanner sc = new Scanner(System.in);
+        int number = 1;
 
         while(true){
             System.out.print("명령) ");
@@ -15,6 +16,16 @@ public class Main {
             if (cmd.equals("종료")){
                 System.out.println("명언 앱을 종료합니다.");
                 break;
+            } else if (cmd.equals("등록")){
+                System.out.print("명언 : ");
+                String saying = sc.nextLine();
+
+                System.out.print("작가 : ");
+                String author = sc.nextLine();
+
+                System.out.println(number + "번 명언이 등록되었습니다.");
+                number++;
+
             }
         }
 
